09653d59ed6cea820ef5907760f41e369959918b
pallavimaruji <36764894+pallavimaruji@users.noreply.github.com>
2018-08-21 17:59:38 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    cin>>a[i];
+    if(n!=0)
+        for(i=0;i<n;i++)
+    {
+        for(j=1;j<=n;j++)  1 4 3 2
+        {
+            i=3
+            r=a[i];
+            a[i]==j;
+        //a[i+1]==
+
+    cout << "" << endl;
+    return 0;
+}
